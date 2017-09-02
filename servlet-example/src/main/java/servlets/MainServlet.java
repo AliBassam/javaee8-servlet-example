a13d4bf855056032f2e8b464807a47a5a032254d
Ali Bassam <contact@alibassam.com>
2017-09-02 08:10:07 +0300

    first commit

---
+++ servlet-example/src/main/java/servlets/MainServlet.java
@@ -0,0 +1,24 @@
+package servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns={"/main"})
+public class MainServlet extends HttpServlet {
+
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.getWriter().write("Hello World " + req.getServletContext().getMajorVersion() + "." + req.getServletContext().getMinorVersion());
+	}
+}
